e1917435489e1bb8314bf81d31768e888a6eb4d7
flavio <flavio@flavio-PC>
2014-02-09 15:39:03 -0200

    nova atualizaçao 2014 sabado dia 8 de fevereiro

--- GerFarma/src/br/com/alf/gerfarma/GerFarma.java
+++ GerFarma/src/br/com/alf/gerfarma/GerFarma.java
@@ -2,6 +2,7 @@ package br.com.alf.gerfarma;
 
 import br.com.alf.gerfarma.view.FrmPrincipal;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -12,11 +13,19 @@ import javax.swing.UIManager;
 public class GerFarma {
     
     public static void main(String[] args) {
+        //Coloca as telas com aparência do Windows 7
         try {
            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+        } catch (InstantiationException ex) {
+                System.out.println(ex.getMessage());
+        } catch (IllegalAccessException ex) {
+                System.out.println(ex.getMessage());
+        } catch (UnsupportedLookAndFeelException ex) {
+                System.out.println(ex.getMessage());
         }
+        //Abre a tela principal do sistema
         new FrmPrincipal().setVisible(true);
     }
 }
